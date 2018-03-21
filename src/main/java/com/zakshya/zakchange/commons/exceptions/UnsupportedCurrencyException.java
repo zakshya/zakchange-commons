162150fd06bef969552ac5ef222ae3d03e36383c
hyahyaoui <hassen.yahyaoui@zakshya.com>
2018-03-21 23:48:26 +0100

    Ticker Info

---
+++ src/main/java/com/zakshya/zakchange/commons/exceptions/UnsupportedCurrencyException.java
@@ -0,0 +1,8 @@
+package com.zakshya.zakchange.commons.exceptions;
+
+public class UnsupportedCurrencyException extends RuntimeException {
+
+    public UnsupportedCurrencyException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/zakshya/zakchange/commons/exceptions/UnsupportedCurrencyPairException.java
@@ -0,0 +1,8 @@
+package com.zakshya.zakchange.commons.exceptions;
+
+public class UnsupportedCurrencyPairException extends RuntimeException {
+
+    public UnsupportedCurrencyPairException(String message) {
+        super(message);
+    }
+}
